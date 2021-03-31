60a62c943cdc006102995eb35c0093399cb77c47
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-03-31 11:40:47 +0200

    Company Read/ReadAll is accessable

---
+++ Backend/API/Controllers/CompanyController.cs
@@ -0,0 +1,41 @@
+﻿using BusinessContractLayer;
+using BusinessLogicLayer;
+using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CompanyController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public CompanyController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        [HttpPost("[action]")]
+        public bool Create(string name, [FromBody]List<Movie> movies)
+        {
+            return new CreateCompany(_applicationDbContext).Do(name, movies);
+        }
+
+        [HttpGet("[action]/{id}")]
+        public Company Read(int id)
+        {
+            return new ReadCompany(_applicationDbContext).Do(id);
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<Company> ReadAll()
+        {
+            return new ReadAllCompanies(_applicationDbContext).Do();
+        }
+    }
+}
