3c7b4b6565b516e7d097ec2b526b61623b2bf45c
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-03-31 11:41:09 +0200

    Movie ReadAll is accessable

---
+++ Backend/API/Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessContractLayer;
+using BusinessLogicLayer;
+using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public MovieController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<Movie> ReadAll()
+        {
+            return new ReadAllMovies(_applicationDbContext).Do();
+        }
+    }
+}
---
+++ Backend/BusinessLogicLayer/Movie/ReadAllMovies.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer;
+using BusinessContractLayer;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusinessLogicLayer
+{
+    public class ReadAllMovies
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public ReadAllMovies(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public IEnumerable<Movie> Do()
+        {
+            return _applicationDbContext.Movies.ToList();
+        }
+    }
+}
