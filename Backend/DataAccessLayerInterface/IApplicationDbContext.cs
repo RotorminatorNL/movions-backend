cef36fcc32cb5422df3b616dabfae81e830f1094
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-04-07 20:59:32 +0200

    Added DAL interface

---
+++ Backend/DataAccessLayerInterface/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayerInterface
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Company> Companies { get; set; }
+        DbSet<Gender> Genders { get; set; }
+        DbSet<Genre> Genres { get; set; }
+        DbSet<Language> Languages { get; set; }
+        DbSet<Movie> Movies { get; set; }
+        DbSet<CrewRole> CrewRoles { get; set; }
+        DbSet<Person> Persons { get; set; }
+    }
+}
