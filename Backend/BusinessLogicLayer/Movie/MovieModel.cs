2774db4d37f8cde5964e50203edae4a4ba0d766a
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-04-14 23:09:28 +0200

    Changed Length from DateTime to int

--- Backend/BusinessLogicLayer/Movie/MovieModel.cs
+++ Backend/BusinessLogicLayer/Movie/MovieModel.cs
@@ -8,7 +8,7 @@ namespace BusinessLogicLayer
     {
         public int ID { get; set; }
         public string Description { get; set; }
-        public DateTime Length { get; set; }
+        public int Length { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string Title { get; set; }
         public IEnumerable<CompanyModel> Companies { get; set; }
