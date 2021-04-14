eac793878c3a7e176bb59399af82b7e95d781a44
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-04-14 10:28:43 +0200

    EF Core stuff update

---
+++ Backend/DataAccessLayer/Migrations/20210408085807_DeletedSelfMadeJoinTables.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class DeletedSelfMadeJoinTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovieCompanies");
+
+            migrationBuilder.DropTable(
+                name: "MovieGenres");
+
+            migrationBuilder.CreateTable(
+                name: "CompanyMovie",
+                columns: table => new
+                {
+                    CompaniesID = table.Column<int>(type: "int", nullable: false),
+                    MoviesID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyMovie", x => new { x.CompaniesID, x.MoviesID });
+                    table.ForeignKey(
+                        name: "FK_CompanyMovie_Companies_CompaniesID",
+                        column: x => x.CompaniesID,
+                        principalTable: "Companies",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CompanyMovie_Movies_MoviesID",
+                        column: x => x.MoviesID,
+                        principalTable: "Movies",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GenreMovie",
+                columns: table => new
+                {
+                    GenresID = table.Column<int>(type: "int", nullable: false),
+                    MoviesID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GenreMovie", x => new { x.GenresID, x.MoviesID });
+                    table.ForeignKey(
+                        name: "FK_GenreMovie_Genres_GenresID",
+                        column: x => x.GenresID,
+                        principalTable: "Genres",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GenreMovie_Movies_MoviesID",
+                        column: x => x.MoviesID,
+                        principalTable: "Movies",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyMovie_MoviesID",
+                table: "CompanyMovie",
+                column: "MoviesID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GenreMovie_MoviesID",
+                table: "GenreMovie",
+                column: "MoviesID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompanyMovie");
+
+            migrationBuilder.DropTable(
+                name: "GenreMovie");
+
+            migrationBuilder.CreateTable(
+                name: "MovieCompanies",
+                columns: table => new
+                {
+                    MovieID = table.Column<int>(type: "int", nullable: false),
+                    CompanyID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieCompanies", x => new { x.MovieID, x.CompanyID });
+                    table.ForeignKey(
+                        name: "FK_MovieCompanies_Companies_CompanyID",
+                        column: x => x.CompanyID,
+                        principalTable: "Companies",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MovieCompanies_Movies_MovieID",
+                        column: x => x.MovieID,
+                        principalTable: "Movies",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MovieGenres",
+                columns: table => new
+                {
+                    MovieID = table.Column<int>(type: "int", nullable: false),
+                    GenreID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieGenres", x => new { x.MovieID, x.GenreID });
+                    table.ForeignKey(
+                        name: "FK_MovieGenres_Genres_GenreID",
+                        column: x => x.GenreID,
+                        principalTable: "Genres",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MovieGenres_Movies_MovieID",
+                        column: x => x.MovieID,
+                        principalTable: "Movies",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieCompanies_CompanyID",
+                table: "MovieCompanies",
+                column: "CompanyID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieGenres_GenreID",
+                table: "MovieGenres",
+                column: "GenreID");
+        }
+    }
+}
