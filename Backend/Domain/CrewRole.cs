1e0adb59601bcff82533a6777d484a7f22445311
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-04-15 09:42:51 +0200

    Added Entity/ID of Movie and Person to CrewRole

--- Backend/Domain/CrewRole.cs
+++ Backend/Domain/CrewRole.cs
@@ -9,6 +9,10 @@ namespace Domain
         public int ID { get; set; }
         public string CharacterName { get; set; }
         public Roles Role { get; set; }
+        public int MovieID { get; set; }
+        public Movie Movie { get; set; }
+        public int PersonID { get; set; }
+        public Person Person { get; set; }
 
         public enum Roles
         {
