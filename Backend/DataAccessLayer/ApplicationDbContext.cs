a9ca75b1b66aa98f8b884b1c2ca2a65f14a68671
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-04-20 22:05:22 +0200

    Fixed interface for the DbContext

--- Backend/DataAccessLayer/ApplicationDbContext.cs
+++ Backend/DataAccessLayer/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext, IApplicationDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) { }
--- Backend/DataAccessLayerInterface/IApplicationDbContext.cs
+++ Backend/DataAccessLayerInterface/IApplicationDbContext.cs
@@ -1,15 +1,19 @@
 ﻿using Domain;
 using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DataAccessLayerInterface
 {
     public interface IApplicationDbContext
     {
-        DbSet<Company> Companies { get; set; }
-        DbSet<Genre> Genres { get; set; }
-        DbSet<Language> Languages { get; set; }
-        DbSet<Movie> Movies { get; set; }
-        DbSet<CrewRole> CrewRoles { get; set; }
-        DbSet<Person> Persons { get; set; }
+        DbSet<Company> Companies { get; }
+        DbSet<Genre> Genres { get; }
+        DbSet<Language> Languages { get; }
+        DbSet<Movie> Movies { get; }
+        DbSet<CrewRole> CrewRoles { get; }
+        DbSet<Person> Persons { get; }
+
+        Task SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
     }
 }
