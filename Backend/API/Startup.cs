16e43dfc5fd8033bec1e8faace23c13e2207b347
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-04-20 22:21:57 +0200

    Added IAppDbContext to services

--- Backend/API/Startup.cs
+++ Backend/API/Startup.cs
@@ -1,4 +1,5 @@
 using DataAccessLayer;
+using DataAccessLayerInterface;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -20,7 +21,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(
+            services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(
                 options => options.UseSqlServer(
                     Configuration.GetConnectionString("MovionsDB"), 
                     b => b.MigrationsAssembly("DataAccessLayer")
