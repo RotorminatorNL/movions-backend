05e33b86f9f273bd2e0bbeffa6d107617afa27d3
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-04-28 17:02:09 +0200

    Add: UnitTest project with example unit test using xunit

---
+++ Backend/UnitTests/Application/MovieTests.cs
@@ -0,0 +1,43 @@
+﻿using Application;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests
+{
+    public class MovieTests
+    {
+        private readonly DbContextOptions<ApplicationDbContext> _dbContextOptions;
+
+        public MovieTests()
+        {
+            _dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "movions_movies")
+                .Options;
+        }
+
+        public async Task ReadAll()
+        {
+            // Arrange 
+            var dbContext = new ApplicationDbContext(_dbContextOptions);
+
+            dbContext.Movies.AddRange(
+                Enumerable.Range(1, 5).Select(m => new Domain.Movie { ID = m, Description = $"Description {m}" })
+            );
+
+            await dbContext.SaveChangesAsync();
+
+            var movie = new Movie(dbContext);
+
+            // Act
+            var result = movie.ReadAll();
+
+            // Assert
+            var movieModel = Assert.IsAssignableFrom<IEnumerable<MovieModel>>(result);
+            Assert.Equal(5, movieModel.Count());
+        }
+    }
+}
