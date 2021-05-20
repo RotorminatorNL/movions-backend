3e84ddd4312ad49e98e2946de14c45bb643bf1cc
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-05-20 12:53:20 +0200

    Add setup tests

--- Backend/UnitTests/Application/CompanyTests.cs
+++ Backend/UnitTests/Application/CompanyTests.cs
@@ -94,5 +94,76 @@ namespace UnitTests
             Assert.Null(actualCompany);
             #endregion
         }
+
+        [Theory]
+        [InlineData(1)]
+        public async Task Read_ValidInput_ReturnsCorrectData(int id)
+        {
+
+        }
+
+        [Theory]
+        [InlineData(0)]
+        public async Task Read_InvalidInput_ReturnsNull(int id)
+        {
+
+        }
+
+        [Fact]
+        public async Task ReadAll_ReturnsAllCompanies()
+        {
+
+        }
+
+        [Theory]
+        [InlineData(1, "Name", CompanyTypes.Distributor)]
+        public async Task Update_ValidInput_ReturnsCorrectData(int id, string name, CompanyTypes companyType)
+        {
+
+        }
+
+        public static IEnumerable<object[]> UpdateInvalidInputData()
+        {
+            int id = 1;
+            string name = "Name";
+            CompanyTypes companyType = CompanyTypes.Distributor;
+
+            // id = 0
+            yield return new object[] { 0, name, companyType };
+            // name = null
+            yield return new object[] { id, null, companyType };
+            // name = empty
+            yield return new object[] { id, "", companyType };
+            // companyType = 100 (does not exist)
+            yield return new object[] { id, name, 100 };
+        }
+
+        [Theory]
+        [MemberData(nameof(UpdateInvalidInputData))]
+        public async Task Update_InvalidInput_ReturnsNull(int id, string name, CompanyTypes companyType)
+        {
+
+        }
+
+        [Theory]
+        [InlineData(1, "Name", CompanyTypes.Distributor)]
+        public async Task Update_InputIsNotDifferent_ReturnsEmptyAdminCompanyModel(int id, string name, CompanyTypes companyType)
+        {
+
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public async Task Delete_ValidInput_ReturnsTrue(int id)
+        {
+
+        }
+
+        [Theory]
+        [InlineData(0)]
+        public async Task Delete_InvalidInput_ReturnsFalse(int id)
+        {
+
+        }
     }
 }
