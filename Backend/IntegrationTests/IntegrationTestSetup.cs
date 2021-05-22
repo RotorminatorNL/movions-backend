3c57753e432f0dd7a790e207de0720dee05e693c
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-05-22 15:29:39 +0200

    Add CreateHttpClient

--- Backend/IntegrationTests/IntegrationTestSetup.cs
+++ Backend/IntegrationTests/IntegrationTestSetup.cs
@@ -2,28 +2,36 @@
 using Microsoft.Extensions.DependencyInjection;
 using PersistenceInterface;
 using System;
+using System.Net.Http;
 using Xunit;
 
 namespace IntegrationTests
 {
     public class IntegrationTestSetup : IClassFixture<ApiFactory<Startup>>, IDisposable
     {
-        private readonly ApiFactory<Startup> _factory;
+        private readonly ApiFactory<Startup> _apiFactory;
         private readonly IServiceScopeFactory _serviceScopeFactory;
         private IServiceScope _serviceScope;
 
         public IntegrationTestSetup(ApiFactory<Startup> factory)
         {
-            _factory = factory;
+            _apiFactory = factory;
             _serviceScopeFactory = factory.Services.GetService<IServiceScopeFactory>();
         }
 
-        protected IApplicationDbContext GetNewDbContext()
+        protected IApplicationDbContext CreateDbContext()
         {
             _serviceScope = _serviceScopeFactory?.CreateScope();
             var dbContext = _serviceScope.ServiceProvider.GetService<IApplicationDbContext>();
 
             return dbContext;
+        }
+
+        protected HttpClient CreateHttpClient()
+        {
+            var client = _apiFactory.CreateClient();
+
+            return client;
         }
 
         public void Dispose()
