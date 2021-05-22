a716f586a6b80494c8e508681a89a1053473a8a2
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-05-22 13:46:12 +0200

    Add integration test setup

---
+++ Backend/IntegrationTests/IntegrationTestSetup.cs
@@ -0,0 +1,34 @@
+﻿using API;
+using Microsoft.Extensions.DependencyInjection;
+using PersistenceInterface;
+using System;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class IntegrationTestSetup : IClassFixture<ApiFactory<Startup>>, IDisposable
+    {
+        private readonly ApiFactory<Startup> _factory;
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+        private IServiceScope _serviceScope;
+
+        public IntegrationTestSetup(ApiFactory<Startup> factory)
+        {
+            _factory = factory;
+            _serviceScopeFactory = factory.Services.GetService<IServiceScopeFactory>();
+        }
+
+        protected IApplicationDbContext GetNewDbContext()
+        {
+            _serviceScope = _serviceScopeFactory?.CreateScope();
+            var dbContext = _serviceScope.ServiceProvider.GetService<IApplicationDbContext>();
+
+            return dbContext;
+        }
+
+        public void Dispose()
+        {
+            _serviceScope.Dispose();
+        }
+    }
+}
---
+++ Backend/IntegrationTests/Tests/CompanyControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTests
+{
+    public class CompanyControllerTests : IntegrationTestSetup
+    {
+        public CompanyControllerTests(ApiFactory<Startup> factory)
+            : base(factory)
+        { }
+    }
+}
