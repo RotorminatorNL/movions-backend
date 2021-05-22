09d94c113b4927cce5c76391441ee173e144cc6e
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-05-22 15:30:03 +0200

    Add first test - Company create

--- Backend/IntegrationTests/Tests/CompanyControllerTests.cs
+++ Backend/IntegrationTests/Tests/CompanyControllerTests.cs
@@ -1,9 +1,11 @@
 ﻿using API;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Application;
+using Domain.Enums;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace IntegrationTests
 {
@@ -12,5 +14,35 @@ namespace IntegrationTests
         public CompanyControllerTests(ApiFactory<Startup> factory)
             : base(factory)
         { }
+
+        [Theory]
+        [InlineData("Name", CompanyTypes.Distributor)]
+        public async Task Create_ValidInput_ReturnsJsonResponseAndCreated(string name, CompanyTypes companyType)
+        {
+            #region Arrange 
+            var dbContent = CreateDbContext();
+
+            var client = CreateHttpClient();
+            var expectedCompany = new AdminCompanyModel
+            {
+                ID = 1,
+                Name = name,
+                Type = companyType
+            };
+            #endregion
+
+            #region Act
+            var response = await client.PostAsJsonAsync("/company/create", expectedCompany);
+            var responseBody = await response.Content.ReadAsStreamAsync();
+            var actualCompany = await JsonSerializer.DeserializeAsync<AdminCompanyModel>(responseBody);
+            #endregion
+
+            #region Assert
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            Assert.Equal(expectedCompany.ID, actualCompany.ID);
+            Assert.Equal(expectedCompany.Name, actualCompany.Name);
+            Assert.Equal(expectedCompany.Type, actualCompany.Type);
+            #endregion
+        }
     }
 }
