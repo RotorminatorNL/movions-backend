567c0c9fc084bb67378b237bc3e1859b5bd27754
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-05-25 00:27:10 +0200

    Add invalid input test

--- Backend/IntegrationTests/Tests/CompanyControllerTests.cs
+++ Backend/IntegrationTests/Tests/CompanyControllerTests.cs
@@ -1,6 +1,8 @@
 ﻿using API;
 using Application;
 using Domain.Enums;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
@@ -16,13 +18,14 @@ namespace IntegrationTests
         { }
 
         [Theory]
-        [InlineData("Name", CompanyTypes.Distributor)]
+        [InlineData("Name", CompanyTypes.Producer)]
         public async Task Create_ValidInput_ReturnsJsonResponseAndCreated(string name, CompanyTypes companyType)
         {
             #region Arrange 
-            var dbContent = CreateDbContext().Companies;
+            await DeleteDbContent();
 
             var client = CreateHttpClient();
+
             var expectedCompany = new AdminCompanyModel
             {
                 ID = 1,
@@ -38,10 +41,56 @@ namespace IntegrationTests
             #endregion
 
             #region Assert
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
             Assert.Equal(expectedCompany.ID, actualCompany.ID);
             Assert.Equal(expectedCompany.Name, actualCompany.Name);
             Assert.Equal(expectedCompany.Type, actualCompany.Type);
+            #endregion
+        }
+
+        public static IEnumerable<object[]> CreateInvalidInputData()
+        {
+            string name = "Name";
+            CompanyTypes companyType = CompanyTypes.Distributor;
+
+            // both = null
+            yield return new object[] { null, null, new string[] { "Name", "CompanyType" } };
+            // name = null
+            yield return new object[] { null, companyType, new string[] { "Name" } };
+            // name = empty
+            yield return new object[] { "", companyType, new string[] { "Name" } };
+            // companyType = 100 (does not exists)
+            yield return new object[] { name, 100, new string[] { "CompanyType" } };
+        }
+
+        [Theory]
+        [MemberData(nameof(CreateInvalidInputData))]
+        public async Task Create_InvalidInput_ReturnsJsonResponseAndBadRequest(string name, CompanyTypes companyType, string[] expectedErrors)
+        {
+            #region Arrange 
+            await DeleteDbContent();
+
+            var client = CreateHttpClient();
+
+            var invalidCompanyData = new AdminCompanyModel
+            {
+                ID = 1,
+                Name = name,
+                Type = companyType
+            };
+            #endregion
+
+            #region Act
+            var response = await client.PostAsJsonAsync("/company/create", invalidCompanyData);
+            var responseBody = await response.Content.ReadAsStreamAsync();
+            var actualCompany = await JsonSerializer.DeserializeAsync<JsonElement>(responseBody);
+            #endregion
+
+            #region Assert
+            var errors = actualCompany.GetProperty("errors").EnumerateObject();
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal(expectedErrors.Length, errors.Count());
             #endregion
         }
     }
