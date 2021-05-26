11e40851fb997210dce41b94306b484606837aff
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-05-26 21:42:03 +0200

    Update route | Add remaining tests

--- Backend/IntegrationTests/Tests/CompanyControllerTests.cs
+++ Backend/IntegrationTests/Tests/CompanyControllerTests.cs
@@ -35,7 +35,7 @@ namespace IntegrationTests
             #endregion
 
             #region Act
-            var response = await client.PostAsJsonAsync("/company/create", expectedCompany);
+            var response = await client.PostAsJsonAsync("/api/company", expectedCompany);
             var responseBody = await response.Content.ReadAsStreamAsync();
             var actualCompany = await JsonSerializer.DeserializeAsync<AdminCompanyModel>(responseBody);
             #endregion
@@ -89,8 +89,8 @@ namespace IntegrationTests
 
             #region Act
             var response = name == "wrongModel" 
-                ? await client.PostAsJsonAsync("/company/create", wrongModel) 
-                : await client.PostAsJsonAsync("/company/create", invalidCompanyData);
+                ? await client.PostAsJsonAsync("/api/company", wrongModel) 
+                : await client.PostAsJsonAsync("/api/company", invalidCompanyData);
             var responseBody = await response.Content.ReadAsStreamAsync();
             var actualCompany = await JsonSerializer.DeserializeAsync<JsonElement>(responseBody);
 
@@ -131,7 +131,7 @@ namespace IntegrationTests
             #endregion
 
             #region Act
-            var response = await client.GetAsync($"/company/read/{id}");
+            var response = await client.GetAsync($"/api/company/{id}");
             var responseBody = await response.Content.ReadAsStreamAsync();
             var actualCompany = await JsonSerializer.DeserializeAsync<CompanyModel>(responseBody);
             #endregion
@@ -164,7 +164,7 @@ namespace IntegrationTests
             #endregion
 
             #region Act
-            var response = await client.GetAsync($"/company/read/{id}");
+            var response = await client.GetAsync($"/api/company/{id}");
             #endregion
 
             #region Assert
@@ -197,7 +197,7 @@ namespace IntegrationTests
             #endregion
 
             #region Act
-            var response = await client.GetAsync("/company/readall");
+            var response = await client.GetAsync("/api/company");
             var responseBody = await response.Content.ReadAsStreamAsync();
             var actualCompanies = await JsonSerializer.DeserializeAsync<IEnumerable<CompanyModel>>(responseBody);
             #endregion
@@ -219,11 +219,206 @@ namespace IntegrationTests
             #endregion
 
             #region Act
-            var response = await client.GetAsync("/company/readall");
+            var response = await client.GetAsync("/api/company");
             #endregion
 
             #region Assert
             Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+            #endregion
+        }
+
+        [Theory]
+        [InlineData(1, "Some other name", CompanyTypes.Producer)]
+        public async Task Update_ValidRequest_ReturnsJsonResponseAndOk(int id, string name, CompanyTypes companyType)
+        {
+            #region Arrange 
+            await DeleteDbContent();
+
+            var client = CreateHttpClient();
+            var dbContext = GetDbContext();
+
+            dbContext.Companies.Add(new Domain.Company
+            {
+                Name = "Name",
+                Type = CompanyTypes.Distributor
+            });
+            await dbContext.SaveChangesAsync();
+
+            var expectedCompany = new AdminCompanyModel
+            {
+                ID = id,
+                Name = name,
+                Type = companyType
+            };
+            #endregion
+
+            #region Act
+            var response = await client.PutAsJsonAsync($"/api/company/{expectedCompany.ID}", expectedCompany);
+            var responseBody = await response.Content.ReadAsStreamAsync();
+            var actualCompany = await JsonSerializer.DeserializeAsync<AdminCompanyModel>(responseBody);
+            #endregion
+
+            #region Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(expectedCompany.ID, actualCompany.ID);
+            Assert.Equal(expectedCompany.Name, actualCompany.Name);
+            Assert.Equal(expectedCompany.Type, actualCompany.Type);
+            #endregion
+        }
+
+        public static IEnumerable<object[]> UpdateInvalidRequestData()
+        {
+            int id = 1;
+            string newName = "Some other name";
+            CompanyTypes newCompanyType = CompanyTypes.Producer;
+
+            // object = null
+            yield return new object[] { 0, "null", 0, new string[] { "" } };
+            // object = wrong
+            yield return new object[] { 0, "wrongModel", 0, new string[] { "$.type" } };
+            // name = null
+            yield return new object[] { id, null, newCompanyType, new string[] { "Name" } };
+            // name = empty
+            yield return new object[] { id, "", newCompanyType, new string[] { "Name" } };
+            // companyType = 100 (does not exist)
+            yield return new object[] { id, newName, 100, new string[] { "Type" } };
+            // name = null | companyType = 100 (does not exist)
+            yield return new object[] { id, null, 100, new string[] { "Name", "Type" } };
+        }
+
+        [Theory]
+        [MemberData(nameof(UpdateInvalidRequestData))]
+        public async Task Update_InvalidRequest_ReturnsJsonResponseAndBadRequest(int id, string name, CompanyTypes companyType, IEnumerable<string> expectedErrors)
+        {
+            #region Arrange 
+            await DeleteDbContent();
+
+            var client = CreateHttpClient();
+            var dbContext = GetDbContext();
+
+            dbContext.Companies.Add(new Domain.Company
+            {
+                Name = "Name",
+                Type = CompanyTypes.Distributor
+            });
+            await dbContext.SaveChangesAsync();
+
+            var expectedCompany = new AdminCompanyModel
+            {
+                ID = id,
+                Name = name,
+                Type = companyType
+            };
+            expectedCompany = expectedCompany.Name == "null" ? null : expectedCompany;
+
+            var wrongModel = new CompanyModel { ID = id, Name = name, Type = companyType.ToString() };
+            #endregion
+
+            #region Act
+            var response = name == "wrongModel" 
+                ? await client.PutAsJsonAsync($"/api/company/{id}", wrongModel)
+                : await client.PutAsJsonAsync($"/api/company/{id}", expectedCompany);
+            var responseBody = await response.Content.ReadAsStreamAsync();
+            var actualCompany = await JsonSerializer.DeserializeAsync<JsonElement>(responseBody);
+
+            var errorProp = actualCompany.GetProperty("errors");
+            var errors = errorProp.EnumerateObject();
+            #endregion
+
+            #region Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal(expectedErrors.Count(), errors.Count());
+            Assert.All(expectedErrors, error => Assert.Contains(error, errors.Select(prop => prop.Name)));
+            #endregion
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(2)]
+        public async Task Update_InvalidRequest_ReturnsJsonResponseAndNotFound(int id)
+        {
+            #region Arrange 
+            await DeleteDbContent();
+
+            var client = CreateHttpClient();
+            var dbContext = GetDbContext();
+
+            dbContext.Companies.Add(new Domain.Company
+            {
+                Name = "Name",
+                Type = CompanyTypes.Distributor
+            });
+            await dbContext.SaveChangesAsync();
+
+            var expectedCompany = new AdminCompanyModel
+            {
+                ID = id,
+                Name = "Some other name",
+                Type = CompanyTypes.Producer
+            };
+            #endregion
+
+            #region Act
+            var response = await client.PutAsJsonAsync($"/api/company/{id}", expectedCompany);
+            #endregion
+
+            #region Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+            #endregion
+        }
+
+        [Theory]
+        [InlineData(1)]
+        public async Task Delete_ValidRequest_ReturnsJsonResponseAndOk(int id)
+        {
+            #region Arrange 
+            await DeleteDbContent();
+
+            var client = CreateHttpClient();
+            var dbContext = GetDbContext();
+
+            dbContext.Companies.Add(new Domain.Company
+            {
+                Name = "Name",
+                Type = CompanyTypes.Distributor
+            });
+            await dbContext.SaveChangesAsync();
+            #endregion
+
+            #region Act
+            var response = await client.DeleteAsync($"/api/company/{id}");
+            #endregion
+
+            #region Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            #endregion
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(2)]
+        public async Task Delete_InvalidRequest_ReturnsJsonResponseAndNotFound(int id)
+        {
+            #region Arrange 
+            await DeleteDbContent();
+
+            var client = CreateHttpClient();
+            var dbContext = GetDbContext();
+
+            dbContext.Companies.Add(new Domain.Company
+            {
+                Name = "Name",
+                Type = CompanyTypes.Distributor
+            });
+            await dbContext.SaveChangesAsync();
+            #endregion
+
+            #region Act
+            var response = await client.DeleteAsync($"/api/company/{id}");
+            #endregion
+
+            #region Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
             #endregion
         }
     }
