a170e436cccd93d38e05babb2a08bdb4cc4dbe13
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-05-29 17:20:11 +0200

    Moved test

--- Backend/UnitTests/CompanyTests.cs
+++ Backend/UnitTests/CompanyTests.cs
@@ -100,113 +100,6 @@ namespace UnitTests
         }
 
         [Theory]
-        [InlineData(1, 1)]
-        public async Task AddMovie_ValidInput_ReturnsCorrectData(int id, int movieID)
-        {
-            #region Arrange
-            var dbContext = new ApplicationDbContext(_dbContextOptions);
-            await dbContext.Database.EnsureDeletedAsync();
-
-            var newCompany = new Domain.Company
-            {
-                Name = "Epic Producer",
-                Type = CompanyTypes.Producer
-            };
-
-            var newMovie = new Domain.Movie
-            {
-                Title = "Epic Title"
-            };
-
-            dbContext.Companies.Add(newCompany);
-            dbContext.Movies.Add(newMovie);
-            await dbContext.SaveChangesAsync();
-
-            var companyMovie = new AdminCompanyMovieModel
-            {
-                CompanyID = id,
-                MovieID = movieID
-            };
-
-            var expectedCompany = new CompanyModel
-            {
-                ID = 1,
-                Name = "Epic Producer",
-                Type = CompanyTypes.Producer.ToString(),
-                Movies = new List<MovieModel> 
-                {
-                    new MovieModel
-                    {
-                        ID = 1,
-                        Title = "Epic Title"
-                    }
-                }
-            };
-
-            var appCompany = new Company(dbContext);
-            #endregion
-
-            #region Act
-            var actualCompany = await appCompany.ConnectMovie(companyMovie);
-            #endregion
-
-            #region Assert
-            Assert.Equal(expectedCompany.ID, actualCompany.ID);
-            Assert.Equal(expectedCompany.Name, actualCompany.Name);
-            Assert.Equal(expectedCompany.Type, actualCompany.Type);
-            Assert.Equal(expectedCompany.Movies.Count(), actualCompany.Movies.Count());
-            Assert.Equal(expectedCompany.Movies.ToList()[0].ID, actualCompany.Movies.ToList()[0].ID);
-            Assert.Equal(expectedCompany.Movies.ToList()[0].Title, actualCompany.Movies.ToList()[0].Title);
-            #endregion
-        }
-
-        [Theory]
-        [InlineData(0, 0)]
-        [InlineData(0, 1)]
-        [InlineData(1, 0)]
-        [InlineData(1, 2)]
-        [InlineData(2, 1)]
-        [InlineData(2, 2)]
-        public async Task AddMovie_InvalidInput_ReturnsNull(int id, int movieID)
-        {
-            #region Arrange
-            var dbContext = new ApplicationDbContext(_dbContextOptions);
-            await dbContext.Database.EnsureDeletedAsync();
-
-            var newCompany = new Domain.Company
-            {
-                Name = "Epic Producer",
-                Type = CompanyTypes.Producer
-            };
-
-            var newMovie = new Domain.Movie
-            {
-                Title = "Epic Title"
-            };
-
-            dbContext.Companies.Add(newCompany);
-            dbContext.Movies.Add(newMovie);
-            await dbContext.SaveChangesAsync();
-
-            var companyMovie = new AdminCompanyMovieModel
-            {
-                CompanyID = id,
-                MovieID = movieID
-            };
-
-            var appCompany = new Company(dbContext);
-            #endregion
-
-            #region Act
-            var actualCompany = await appCompany.ConnectMovie(companyMovie);
-            #endregion
-
-            #region Assert
-            Assert.Null(actualCompany);
-            #endregion
-        }
-
-        [Theory]
         [InlineData(1)]
         public async Task Read_ValidInput_ReturnsCorrectData(int id)
         {
@@ -412,6 +305,113 @@ namespace UnitTests
 
             #region Act
             var actualCompany = await appCompany.Update(newCompany);
+            #endregion
+
+            #region Assert
+            Assert.Null(actualCompany);
+            #endregion
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        public async Task ConnectMovie_ValidInput_ReturnsCorrectData(int id, int movieID)
+        {
+            #region Arrange
+            var dbContext = new ApplicationDbContext(_dbContextOptions);
+            await dbContext.Database.EnsureDeletedAsync();
+
+            var newCompany = new Domain.Company
+            {
+                Name = "Epic Producer",
+                Type = CompanyTypes.Producer
+            };
+
+            var newMovie = new Domain.Movie
+            {
+                Title = "Epic Title"
+            };
+
+            dbContext.Companies.Add(newCompany);
+            dbContext.Movies.Add(newMovie);
+            await dbContext.SaveChangesAsync();
+
+            var companyMovie = new AdminCompanyMovieModel
+            {
+                CompanyID = id,
+                MovieID = movieID
+            };
+
+            var expectedCompany = new CompanyModel
+            {
+                ID = 1,
+                Name = "Epic Producer",
+                Type = CompanyTypes.Producer.ToString(),
+                Movies = new List<MovieModel>
+                {
+                    new MovieModel
+                    {
+                        ID = 1,
+                        Title = "Epic Title"
+                    }
+                }
+            };
+
+            var appCompany = new Company(dbContext);
+            #endregion
+
+            #region Act
+            var actualCompany = await appCompany.ConnectMovie(companyMovie);
+            #endregion
+
+            #region Assert
+            Assert.Equal(expectedCompany.ID, actualCompany.ID);
+            Assert.Equal(expectedCompany.Name, actualCompany.Name);
+            Assert.Equal(expectedCompany.Type, actualCompany.Type);
+            Assert.Equal(expectedCompany.Movies.Count(), actualCompany.Movies.Count());
+            Assert.Equal(expectedCompany.Movies.ToList()[0].ID, actualCompany.Movies.ToList()[0].ID);
+            Assert.Equal(expectedCompany.Movies.ToList()[0].Title, actualCompany.Movies.ToList()[0].Title);
+            #endregion
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(0, 1)]
+        [InlineData(1, 0)]
+        [InlineData(1, 2)]
+        [InlineData(2, 1)]
+        [InlineData(2, 2)]
+        public async Task ConnectMovie_InvalidInput_ReturnsNull(int id, int movieID)
+        {
+            #region Arrange
+            var dbContext = new ApplicationDbContext(_dbContextOptions);
+            await dbContext.Database.EnsureDeletedAsync();
+
+            var newCompany = new Domain.Company
+            {
+                Name = "Epic Producer",
+                Type = CompanyTypes.Producer
+            };
+
+            var newMovie = new Domain.Movie
+            {
+                Title = "Epic Title"
+            };
+
+            dbContext.Companies.Add(newCompany);
+            dbContext.Movies.Add(newMovie);
+            await dbContext.SaveChangesAsync();
+
+            var companyMovie = new AdminCompanyMovieModel
+            {
+                CompanyID = id,
+                MovieID = movieID
+            };
+
+            var appCompany = new Company(dbContext);
+            #endregion
+
+            #region Act
+            var actualCompany = await appCompany.ConnectMovie(companyMovie);
             #endregion
 
             #region Assert
