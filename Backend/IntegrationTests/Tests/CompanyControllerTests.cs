7389b3acc898ad963633276b23b85cc3c2dc4bb4
Robbert Lengton <robbert.lengton@hotmail.nl>
2021-06-02 13:40:30 +0200

    Remove unused using

--- Backend/IntegrationTests/Tests/CompanyControllerTests.cs
+++ Backend/IntegrationTests/Tests/CompanyControllerTests.cs
@@ -5,7 +5,6 @@ using Domain.Enums;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
